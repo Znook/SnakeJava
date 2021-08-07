341396b59bcc1cd2bdfedb1864fb1ce1d9a7e30d
Znook <Jacob.K289@outlook.com>
2021-08-08 00:53:51 +0200

    added to gameframe

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,6 +3,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel implements ActionListener {
+    GameFrame(){
+
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
 
